d42cd56eab7faa5c39d2845bafdc8cc67a09084b
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-01-11 16:55:09 +0900

    Add input type and relation sample

--- src/index.js
+++ src/index.js
@@ -1,10 +1,71 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const photos = [];
-let idIndex = 0;
+const photos = [
+  {
+    id: "0",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake"
+  },
+  {
+    id: "1",
+    name: "Enjoying the subshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt"
+  },
+  {
+    id: "2",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt"
+  }
+];
+
+const users = [
+  {
+    githubLogin: "mHattrup",
+    name: "Mike Hattrup"
+  },
+  {
+    githubLogin: "gPlake",
+    name: "Glen Plake"
+  },
+  {
+    githubLogin: "sSchmidt",
+    name: "Scot Schmidt"
+  }
+];
+
+let photoIdIndex = photos.length;
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
+  input PostPhotoInput {
+    "Name of the new photo"
+    name: String!
+    "Category of the new photo, default is PORTRAIT"
+    category: PhotoCategory = PORTRAIT
+    "Description of the new photo"
+    description: String
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]
+  }
+
   type Photo {
     "Id of the photo"
     id: ID!
@@ -12,8 +73,12 @@ const typeDefs = gql`
     url: String!
     "Name of the photo"
     name: String!
+    "Category of the photo"
+    category: PhotoCategory
     "Description of the photo"
     description: String
+    "User who added the pohto"
+    postedBy: User!
   }
 
   type Query {
@@ -29,12 +94,7 @@ const typeDefs = gql`
     """
     Add a new photo
     """
-    postPhoto(
-      "Name of the new photo"
-      name: String!,
-      "Description of the new photo"
-      description: String
-    ): Photo!
+    postPhoto(input: PostPhotoInput): Photo!
   }
 `;
 
@@ -52,8 +112,8 @@ const resolvers = {
       console.log(args);
 
       const newPhoto = {
-        id: idIndex++,
-        ...args,
+        id: photoIdIndex++,
+        ...args.input,
       };
 
       photos.push(newPhoto);
@@ -62,7 +122,15 @@ const resolvers = {
   },
   // Type Resolver
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser);
+    }
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin);
+    }
   }
 };
 
