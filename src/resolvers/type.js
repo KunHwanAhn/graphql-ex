a3d9dbb020f279bfb367e6836271f7f8c92179c1
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-01-18 23:35:54 +0900

    오타 수정 및 명확한 이름 사용

--- src/resolvers/type.js
+++ src/resolvers/type.js
@@ -6,13 +6,13 @@ module.exports = {
     id: parent => parent._id,
     url: parent => `http://yoursite.com/img/${parent._id}.jpg`,
     async postedBy(parent, args, { db }) {
-      return await db.collection('users').findOne({ githubLogin: parent.userID })
+      return await db.collection('users').findOne({ githubLogin: parent.userId })
     },
     async taggedUsers(parent, args, { db }) {
       const tags = await getCollection(db, 'tags', { photoId: parent._id })
       const users = await getCollection(db, 'users')
 
-      return tags.map(tag => tag.userId).map(userId => users.find(u => u.githubLogin === userId))
+      return tags.map(tag => tag.userId).map(userId => users.find(user => user.githubLogin === userId))
     },
   },
   User: {
@@ -23,7 +23,7 @@ module.exports = {
       const tags = await getCollection(db, 'tags', { userId: parent.id })
       const photos = await getCollection(db, 'photos')
 
-      return tags.map(tag => tag.photoId).map(photoId => photos.find(p => p.id === photoId))
+      return tags.map(tag => tag.photoId).map(photoId => photos.find(photo => photo.id === photoId))
     },
   },
   DateTime: new GraphQLScalarType({
