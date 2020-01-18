a5136bd83d00edcc971bccd2215875fde15280df
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-01-19 00:54:02 +0900

    Github Auth 연동 추가

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -2,10 +2,17 @@ const { getCollection } = require('./utils')
 
 module.exports = {
   hello: () => 'Hello world!',
+  me: (parent, args, { currentUser }) => currentUser,
   async totalPhotos(parent, args, { db }) {
     return await db.collection('photos').estimatedDocumentCount()
   },
   async allPhotos(parent, args, { db }) {
     return await getCollection(db, 'photos')
+  },
+  async totalUsers(parent, args, { db }) {
+    return await db.collection('users').estimatedDocumentCount()
+  },
+  async allUsers(parent, args, { db }) {
+    return await getCollection(db, 'users')
   },
 }
